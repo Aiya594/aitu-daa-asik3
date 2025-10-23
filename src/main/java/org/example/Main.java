0455a1d9be63be35f44e5943726304bd5ca8a22d
aiya <aichkaxxx2007@gmail.com>
2025-10-23 12:35:41 +0500

    feat(main):implemented logic for main part

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,70 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.example.algorithms.kruskal.Kruskal;
+import org.example.algorithms.prims.Prim;
+import org.example.graph.Graph;
+import org.example.io.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            System.err.println("Usage: java -jar mst-assignment.jar <input.json> <output.json>");
+            System.exit(2);
         }
+
+        File in = Path.of(args[0]).toFile();
+        File out = Path.of(args[1]).toFile();
+
+        ObjectMapper om = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+
+
+        Input input = om.readValue(in, Input.class);
+        List<Output.Result> results = new ArrayList<>();
+
+
+        for (Input.Graphs gEntry : input.graphs) {
+            Graph graph = new Graph(gEntry.nodes, gEntry.edges);
+            // Prim
+            Prim prim = new Prim(graph);
+            prim.runPrim();
+            // Kruskal
+            Kruskal kruskal = new Kruskal(graph);
+            kruskal.runKruskal();
+
+            Output.AlgorithmResult primRes = new Output.AlgorithmResult(
+                    prim.getMstEdges(),
+                    prim.getTotalCost(),
+                    prim.getMetrics().totalOpers(),
+                    (double) prim.getTimeMs()
+            );
+            Output.AlgorithmResult krusRes = new Output.AlgorithmResult(
+                    kruskal.getMstEdges(),
+                    kruskal.getTotalCost(),
+                    kruskal.getMetrics().totalOpers(),
+                    (double) kruskal.getTimeMs()
+            );
+            Map<String,Integer> inputStats = Map.of(
+                    "vertices", graph.vertexCount(),
+                    "edges", graph.edgeCount()
+            );
+            results.add(new Output.Result(gEntry.id, inputStats, primRes, krusRes));
+        }
+
+        Output outputModel = new Output(results);
+        om.writeValue(out, outputModel);
+        System.out.println("Wrote results to " + out.getAbsolutePath());
+
+
     }
 }
